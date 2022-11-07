e6c35587ad03a0161b5093936c5e244d5e2b6b2c
jiebaolong <jiebaolong@ctfo.com>
2022-11-07 17:08:37 +0800

    demo

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,6 +1,11 @@
 package com.example;
 
 import com.util.Util;
+import java.util.Arrays;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  * Hello world!
@@ -8,12 +13,65 @@ import com.util.Util;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args )throws ParseException, Exception
     {
-        System.out.println( "Hello World!d" );
-        byte[] bytes = Util.hexToBytes("FEF0");
-        System.out.println(bytes);
-        String str = Util.bytesToHexString(bytes);
+        // System.out.println( "Hello World!d" );
+        // byte[] bytes1 = Util.hexToBytes("FEF0");
+        // // System.out.println(bytes1);
+        // System.out.println(Arrays.toString(bytes1));
+        // String str = Util.bytesToHexString(bytes1);
+        // System.out.println(str);
+        // int result []  = new int[]{0,1,2,3,4};
+
+        // byte[] bytes = Util.toByteArray(result, false);
+        // System.out.println(Arrays.toString(bytes));
+        // int[] result2 = Util.toIntArray(bytes, false);
+        // System.out.println(Arrays.toString(result2));
+
+        // 2654435665
+        // 2147483647
+        // int val3 = 2147483647;
+        // System.out.println(val3+1+1);
+
+        // int val  = (7 >>> 5);
+        // int val2 = (8 << 4);
+        // System.out.println(val);
+        // System.out.println(val2);
+        // System.out.println("sum-3对应的无符号整数：" + Integer.toUnsignedString(val2));
+
+
+        // int k [] = new int[]{0,1,2};
+        // int v [] = new int[]{5,6,7};
+        // int[] result1 = Util.decrypt(v,k);
+        // System.out.println("sum-3对应的无符号整数：" + Integer.toUnsignedString(-1640531527));
+        // int arr3[] = new int[]{-1640531527,-1,2} ;
+        // System.out.println(Arrays.toString(result1));
+        // System.out.printf("%s, %s, %s",Integer.toUnsignedString(result1[0]),Integer.toUnsignedString(result1[1]),Integer.toUnsignedString(result1[2]));
+        
+        JSONObject obj = new JSONObject();
+		
+		obj.put("name", "Pankaj Kumar");
+		obj.put("age", new Integer(32));
+        String jsonStr = JSONObject.toJSONString(obj);
+
+        System.out.println(jsonStr);
+         
+        // String jsonStr = "{\"name\":\"Pankaj Kumar\",\"age\":32}";
+        // String jsonStr = "{\"school\":\"商职\",\"sex\":\"男\",\"name\":\"wjw\",\"age\":22}";
+        // JSONParser parser = new JSONParser();
+
+		// Object jsonObj = parser.parse(jsonStr);
+
+		// JSONObject jsonObject = (JSONObject) jsonObj;
+
+		// String name = (String) jsonObject.get("name");
+		// System.out.println("Name = " + name);
+
+        String str=  Util.encryptStr(jsonStr, "utf-8", "key");
         System.out.println(str);
+
+        // 解密
+        String newJsonStr = Util.decryptStr(str, "utf-8", "key");
+        System.out.println(newJsonStr);
     }
 }
--- demo/src/main/java/com/util/Util.java
+++ demo/src/main/java/com/util/Util.java
@@ -1,6 +1,152 @@
 package com.util;
 
+import java.util.Arrays;
+
 public class Util {
+    // Encrypt data with key.
+    public static byte[] encrypt(byte[] data, byte[] key) {
+        if (data.length == 0) {
+            return data;
+        }
+        return toByteArray(encrypt(toIntArray(data, true), toIntArray(key, false)), false);
+    }
+
+    // Decrypt data with key.
+    public static byte[] decrypt(byte[] data, byte[] key) {
+        if (data.length == 0) {
+            return data;
+        }
+        return toByteArray(decrypt(toIntArray(data, false), toIntArray(key, false)), true);
+    }
+
+    // Encrypt data with key.
+    public static int[] encrypt(int[] v, int[] k) {
+        int n = v.length - 1;
+        if (n < 1) {
+            return v;
+        }
+        if (k.length < 4) {
+            int[] key = new int[4];
+            System.arraycopy(k, 0, key, 0, k.length);
+            k = key;
+        }
+        int z = v[n], y = v[0], delta = 0x9E3779B9, sum = 0, e;
+        int p, q = 6 + 52 / (n + 1);
+        // System.out.println(p);
+        // System.out.println(Arrays.toString(v));
+
+        while (q-- > 0) {
+            sum = sum + delta;
+            e = sum >>> 2 & 3;
+            // System.out.printf("sum:%s, e:%s, k:%s \n",Integer.toUnsignedString(sum),Integer.toUnsignedString(e),Arrays.toString(k));
+            for (p = 0; p < n; p++) {
+
+                // int temp=  (z >>> 5 ^ y << 2) + (y >>> 3 ^ z << 4) ^ (sum ^ y) + (k[p & 3 ^ e] ^ z);
+                // v[p] += temp;
+                // System.out.printf("temp:%s, v[p]:%s\n",Integer.toUnsignedString(temp),Integer.toUnsignedString(v[p]));
+                // System.out.printf("v[p]:%s\n",Integer.toUnsignedString(v[p]));
+                // int temp = (z >>> 5 ^ y << 2) + (y >>> 3 ^ z << 4) ^ (sum ^ y) + (k[p & 3 ^ e] ^ z);
+                // v[p] += temp;
+                // System.out.printf("v[p]:%s, v[p+1]:%s, y:%s, z: %s \n",Integer.toUnsignedString(v[p]),Integer.toUnsignedString(v[p+1]),Integer.toUnsignedString(y),Integer.toUnsignedString(z));
+                y = v[p + 1];
+                // int temp1 = (z >>> 5 ^ y << 2);
+                // int temp2_1 = y >>> 3;
+                // int temp2_2 = z << 4;
+                // int temp2_0 = temp2_1 ^ temp2_2;
+                // System.out.printf("temp2_0:%d, y:%d, z:%d, temp2_1:%d, temp2_2: %d  \n",temp2_0,y,z,temp2_1,temp2_2);
+                
+                // int temp2 = (y >>> 3 ^ z << 4) ;
+                // int temp3 = (sum ^ y);
+                // int temp4 = (k[p & 3 ^ e] ^ z);
+                // int temp0 = temp1 + temp2 ^ temp3 + temp4;
+                // System.out.printf("temp0:%d, temp1:%d, temp2:%d, temp3:%d, temp4: %d \n",temp0,temp1,temp2,temp3,temp4);
+                
+
+                // int temp101 = (z >>> 5 ^ y << 2) + (y >>> 3 ^ z << 4) ;
+                // int temp102 = (sum ^ y) + (k[p & 3 ^ e] ^ z);
+                // int temp100 = temp101 ^ temp102;
+                
+                int temp = (z >>> 5 ^ y << 2) + (y >>> 3 ^ z << 4) ^ (sum ^ y) + (k[p & 3 ^ e] ^ z);
+                // System.out.printf("temp:%d,temp0:%d, temp100: %d \n",temp,temp0,temp100);
+
+                // System.out.printf("v[p]:%d, temp: %d \n",v[p],temp);
+                v[p] = v[p] + temp;
+                z = v[p];
+                // System.out.printf("q:%s,p: %s, sum:%s, e:%s, y:%s, z:%s, v[p]: %s \n", Integer.toUnsignedString(q),Integer.toUnsignedString(p), Integer.toUnsignedString(sum), Integer.toUnsignedString(e), Integer.toUnsignedString(y), Integer.toUnsignedString(z),Integer.toUnsignedString(v[p]));
+                // if (q < 23) {
+                //     break;
+                // }
+            }
+            // System.out.printf("[内部] q:%s,p: %s, sum:%s, e:%s, y:%s, z:%s, v:%s\n", Integer.toUnsignedString(q),Integer.toUnsignedString(p), Integer.toUnsignedString(sum), Integer.toUnsignedString(e), Integer.toUnsignedString(y), Integer.toUnsignedString(z),Arrays.toString(v));
+
+            y = v[0];
+            z = v[n] += (z >>> 5 ^ y << 2) + (y >>> 3 ^ z << 4) ^ (sum ^ y) + (k[p & 3 ^ e] ^ z);
+        }
+        // System.out.printf("[外部] q:%d, sum:%d,  y:%d, z:%d, v:%s\n", q, sum, y, z,Arrays.toString(v));
+        return v;
+    }
+
+    // Decrypt data with key.
+    public static int[] decrypt(int[] v, int[] k) {
+        int n = v.length - 1;
+        if (n < 1) {
+            return v;
+        }
+        if (k.length < 4) {
+            int[] key = new int[4];
+            System.arraycopy(k, 0, key, 0, k.length);
+            k = key;
+        }
+        int z = v[n], y = v[0], delta = 0x9E3779B9, sum, e;
+        int p, q = 6 + 52 / (n + 1);
+        sum = q * delta;
+        while (sum != 0) {
+            e = sum >>> 2 & 3;
+            for (p = n; p > 0; p--) {
+                z = v[p - 1];
+                y = v[p] -= (z >>> 5 ^ y << 2) + (y >>> 3 ^ z << 4) ^ (sum ^ y) + (k[p & 3 ^ e] ^ z);
+            }
+            z = v[n];
+            y = v[0] -= (z >>> 5 ^ y << 2) + (y >>> 3 ^ z << 4) ^ (sum ^ y) + (k[p & 3 ^ e] ^ z);
+            sum = sum - delta;
+        }
+        return v;
+    }
+
+    // Convert byte array to int array.
+    public static int[] toIntArray(byte[] data, boolean includeLength) {
+        int n = (((data.length & 3) == 0) ? (data.length >>> 2) : ((data.length >>> 2) + 1));
+        int[] result;
+        if (includeLength) {
+            result = new int[n + 1];
+            result[n] = data.length;
+        } else {
+            result = new int[n];
+        }
+        n = data.length;
+        for (int i = 0; i < n; i++) {
+            result[i >>> 2] |= (0x000000ff & data[i]) << ((i & 3) << 3);
+        }
+        return result;
+    }
+
+    // Convert int array to byte array
+    public static byte[] toByteArray(int[] data, boolean includeLength) {
+        int n;
+        if (includeLength) {
+            n = data[data.length - 1];
+        } else {
+            n = data.length << 2;
+        }
+
+        byte[] result = new byte[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = (byte) (data[i >>> 2] >>> ((i & 3) << 3));
+        }
+        return result;
+    }
+
+    // 字节数组转换为hex字符串
     public static final String bytesToHexString(byte[] bArray) {
         StringBuffer sb = new StringBuffer(bArray.length);
 
@@ -13,6 +159,7 @@ public class Util {
         return sb.toString();
     }
 
+    // hex字符串转换为字节数组
     public static byte[] hexToBytes(String str) {
         if (str == null) {
             return null;
@@ -31,5 +178,25 @@ public class Util {
             raw[i] = (byte) value;
         }
         return raw;
+    }
+
+    // 使用XXTea 算法加密字符串
+    public static String encryptStr(String plain, String charset, String key) throws Exception {
+        if (plain == null || charset == null || key == null) {
+            return null;
+        }
+        byte[] bytes = encrypt(plain.getBytes(charset), key.getBytes(charset));
+
+        return bytesToHexString(bytes);
+    }
+
+    // 使用XXTea 算法解密字符串
+    public static String decryptStr(String hexStr, String charset, String key) throws Exception {
+        if (hexStr == null || charset == null || key == null) {
+            return null;
+        }
+        byte[] bytes = decrypt(hexToBytes(hexStr), key.getBytes(charset));
+
+        return new String(bytes, charset);
     }
 }
